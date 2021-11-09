309ba7cd2ca8e5349ed327346e8bccd2d90c8193
Vladimir Boborykin <bpost@yandex.ru>
2021-11-09 12:42:33 +0300

    Добавьте файлы проекта.

---
+++ PlanMedProf.Blazor.Server/BlazorApplication.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace PlanMedProf.Blazor.Server {
+    partial class PlanMedProfBlazorApplication {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.module1 = new DevExpress.ExpressApp.SystemModule.SystemModule();
+            this.module2 = new DevExpress.ExpressApp.Blazor.SystemModule.SystemBlazorModule();
+            this.module3 = new PlanMedProf.Module.PlanMedProfModule();
+            this.module4 = new PlanMedProf.Module.Blazor.PlanMedProfBlazorModule();
+            this.securityModule1 = new DevExpress.ExpressApp.Security.SecurityModule();
+            this.objectsModule = new DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule();
+            this.cloneObjectModule = new DevExpress.ExpressApp.CloneObject.CloneObjectModule();
+            this.conditionalAppearanceModule = new DevExpress.ExpressApp.ConditionalAppearance.ConditionalAppearanceModule();
+            this.validationModule = new DevExpress.ExpressApp.Validation.ValidationModule();
+            this.validationBlazorModule = new DevExpress.ExpressApp.Validation.Blazor.ValidationBlazorModule();
+
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+
+            // 
+            // PlanMedProfBlazorApplication
+            // 
+            this.ApplicationName = "PlanMedProf";
+            this.CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema;
+            this.Modules.Add(this.module1);
+            this.Modules.Add(this.module2);
+            this.Modules.Add(this.module3);
+            this.Modules.Add(this.module4);
+            this.Modules.Add(this.securityModule1);
+            this.Modules.Add(this.objectsModule);
+            this.Modules.Add(this.cloneObjectModule);
+            this.Modules.Add(this.conditionalAppearanceModule);
+            this.Modules.Add(this.validationModule);
+            this.Modules.Add(this.validationBlazorModule);
+            this.DatabaseVersionMismatch += new System.EventHandler<DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs>(this.PlanMedProfBlazorApplication_DatabaseVersionMismatch);
+
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
+
+        #endregion
+
+        private DevExpress.ExpressApp.SystemModule.SystemModule module1;
+        private DevExpress.ExpressApp.Blazor.SystemModule.SystemBlazorModule module2;
+        private PlanMedProf.Module.PlanMedProfModule module3;
+        private PlanMedProf.Module.Blazor.PlanMedProfBlazorModule module4;
+        private DevExpress.ExpressApp.Security.SecurityModule securityModule1;
+        private DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule objectsModule;
+        private DevExpress.ExpressApp.CloneObject.CloneObjectModule cloneObjectModule;
+        private DevExpress.ExpressApp.ConditionalAppearance.ConditionalAppearanceModule conditionalAppearanceModule;
+        private DevExpress.ExpressApp.Validation.ValidationModule validationModule;
+        private DevExpress.ExpressApp.Validation.Blazor.ValidationBlazorModule validationBlazorModule;
+    }
+}
---
+++ PlanMedProf.Win/WinApplication.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace PlanMedProf.Win {
+    partial class PlanMedProfWindowsFormsApplication {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.module1 = new DevExpress.ExpressApp.SystemModule.SystemModule();
+            this.module2 = new DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule();
+            this.module3 = new PlanMedProf.Module.PlanMedProfModule();
+            this.module4 = new PlanMedProf.Module.Win.PlanMedProfWindowsFormsModule();
+            this.securityModule1 = new DevExpress.ExpressApp.Security.SecurityModule();
+            this.securityStrategyComplex1 = new DevExpress.ExpressApp.Security.SecurityStrategyComplex();
+            this.securityStrategyComplex1.SupportNavigationPermissionsForTypes = false;
+            this.objectsModule = new DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule();
+            this.cloneObjectModule = new DevExpress.ExpressApp.CloneObject.CloneObjectModule();
+            this.conditionalAppearanceModule = new DevExpress.ExpressApp.ConditionalAppearance.ConditionalAppearanceModule();
+            this.validationModule = new DevExpress.ExpressApp.Validation.ValidationModule();
+            this.validationWindowsFormsModule = new DevExpress.ExpressApp.Validation.Win.ValidationWindowsFormsModule();
+            this.authenticationStandard1 = new DevExpress.ExpressApp.Security.AuthenticationStandard();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            // 
+            // securityStrategyComplex1
+            // 
+            this.securityStrategyComplex1.Authentication = this.authenticationStandard1;
+            this.securityStrategyComplex1.RoleType = typeof(DevExpress.Persistent.BaseImpl.PermissionPolicy.PermissionPolicyRole);
+            // ApplicationUser descends from PermissionPolicyUser and supports OAuth authentication. For more information, refer to the following help topic: https://docs.devexpress.com/eXpressAppFramework/402197
+            // If your application uses PermissionPolicyUser or a custom user type, set the UserType property as follows:
+            this.securityStrategyComplex1.UserType = typeof(PlanMedProf.Module.BusinessObjects.ApplicationUser);
+            // 
+            // securityModule1
+            // 
+            this.securityModule1.UserType = typeof(PlanMedProf.Module.BusinessObjects.ApplicationUser);
+            // 
+            // authenticationStandard1
+            // 
+            this.authenticationStandard1.LogonParametersType = typeof(DevExpress.ExpressApp.Security.AuthenticationStandardLogonParameters);
+            // ApplicationUserLoginInfo is only necessary for applications that use the ApplicationUser user type.
+            // Comment out the following line if using PermissionPolicyUser or a custom user type.
+            this.authenticationStandard1.UserLoginInfoType = typeof(PlanMedProf.Module.BusinessObjects.ApplicationUserLoginInfo);
+            // 
+            // PlanMedProfWindowsFormsApplication
+            // 
+            this.ApplicationName = "PlanMedProf";
+            this.CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema;
+            this.Modules.Add(this.module1);
+            this.Modules.Add(this.module2);
+            this.Modules.Add(this.module3);
+            this.Modules.Add(this.module4);
+            this.Modules.Add(this.securityModule1);
+            this.Security = this.securityStrategyComplex1;
+            this.Modules.Add(this.objectsModule);
+            this.Modules.Add(this.cloneObjectModule);
+            this.Modules.Add(this.conditionalAppearanceModule);
+            this.Modules.Add(this.validationModule);
+            this.Modules.Add(this.validationWindowsFormsModule);
+            this.UseOldTemplates = false;
+            this.DatabaseVersionMismatch += new System.EventHandler<DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs>(this.PlanMedProfWindowsFormsApplication_DatabaseVersionMismatch);
+            this.CustomizeLanguagesList += new System.EventHandler<DevExpress.ExpressApp.CustomizeLanguagesListEventArgs>(this.PlanMedProfWindowsFormsApplication_CustomizeLanguagesList);
+
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
+
+        #endregion
+
+        private DevExpress.ExpressApp.SystemModule.SystemModule module1;
+        private DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule module2;
+        private PlanMedProf.Module.PlanMedProfModule module3;
+        private PlanMedProf.Module.Win.PlanMedProfWindowsFormsModule module4;
+        private DevExpress.ExpressApp.Security.SecurityModule securityModule1;
+        private DevExpress.ExpressApp.Security.SecurityStrategyComplex securityStrategyComplex1;
+        private DevExpress.ExpressApp.Security.AuthenticationStandard authenticationStandard1;
+        private DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule objectsModule;
+        private DevExpress.ExpressApp.CloneObject.CloneObjectModule cloneObjectModule;
+        private DevExpress.ExpressApp.ConditionalAppearance.ConditionalAppearanceModule conditionalAppearanceModule;
+        private DevExpress.ExpressApp.Validation.ValidationModule validationModule;
+        private DevExpress.ExpressApp.Validation.Win.ValidationWindowsFormsModule validationWindowsFormsModule;
+    }
+}
